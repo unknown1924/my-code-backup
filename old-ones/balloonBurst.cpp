ae1d5a056b9e490ad7880009d6cf7507470ed942
deb <debasismandal900@gmail.com>
2020-09-10 02:08:13 +0530

    balloonBurst.cpp added

--- old-ones/balloonBurst.cpp
+++ old-ones/balloonBurst.cpp
@@ -1,36 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> modified(vector<int> a, int i){
-    vector<int> ret;
-    for(int e:a){
-        if(e != i)
-            ret.push_back(e);
-    }
-    return ret;
-}
-int burst(int start, int end, vector<int> a){
+int maxScoreBurst(vector<int> a){
+    int maxScore = 0;
     int n = a.size();
-    if(n <= 0)  return 0;
-    if(n == 1)  return a[0];
-    if(n == 2)  return a[0]*a[1] + max(a[0], a[1]);
-    int sum = 0;
-    //for(int  i = 0; i < n; i++){
-        //sum = max(sum, burst(modified(a, i)));
-    //}
-    for(int  i = start; i <= end; i++){
-        sum = max(sum, a[i]*burst(0, i-1, a)*burst(i+1, n, a));
+
+    a.insert(a.begin(), 1);
+    a.push_back(1);
+
+    vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
+    for(int w = 1; w <= n; w++){
+        for(int Left = 1; Left <= n-w+1; Left++){
+            int Right = Left+w-1;
+            for(int i = Left; i <= Right; i++){
+                dp[Left][Right] = max(dp[Left][Right], a[Left-1]*a[i]*a[Right+1] + dp[Left][i-1] + dp[i+1][Right]);
+            }
+        }
     }
-    return sum;
+
+    maxScore = dp[1][n]; 
+    return maxScore;
 }
 
-int dpburst(vector<int> a){
-    int n = a.size();
-    int dp[n-2] = {0};
-    return 0;
-}
 
 int main(){
     vector<int> a{3,1,5,8};
-    cout << burst(0, 3, a) << endl;
+    cout << maxScoreBurst(a) << endl;
 }
